ec0b973c5de14a1c6312391bbff3312aafa96379
Samandar <samandarnayimjonov@gmail.com>
2025-02-12 20:35:07 +0200

    Done users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from .forms import CustomUserCreationForm, CustomAuthenticationForm, UserProfileForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, FormView, UpdateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.save()
+        login(self.request, user)
+        return response
+
+class UserLoginView(FormView):
+    form_class = CustomAuthenticationForm
+    template_name = 'accounts/login.html'
+    success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(self.request, user)
+            return super().form_valid(form)
+        return self.form_invalid(form)
+
+class ProfileView(LoginRequiredMixin, UpdateView):
+    form_class = UserProfileForm
+    template_name = 'accounts/profile.html'
+    success_url = reverse_lazy('accounts:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user.profile
