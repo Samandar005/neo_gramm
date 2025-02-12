7b2bbcc19897ea6c76d1cf179f125378f1d4e17b
Samandar <samandarnayimjonov@gmail.com>
2025-02-12 21:03:50 +0200

    posts models yozildi va requirements.txt yozildi

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from posts.base_models import BaseModel
+from posts.models import Posts
 
-# Create your models here.
+
+class Comments(BaseModel):
+    comments = models.CharField(max_length=255)
+    posts = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name='comments')
+
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from .base_models import BaseModel
 
-# Create your models here.
+
+class Posts(BaseModel):
+    image = models.ImageField(upload_to='posts/'),
+    caption = models.CharField(max_length=50),
+    hashtags = models.CharField(max_length=50),
+    likes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.caption}"
+
